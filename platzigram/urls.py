bd6096349acd103b73b84e1fd8e0947c839d45e4
Brian olarte <brayan_andrey23@hotmail.com>
2021-03-03 20:55:30 -0500

    Creacion de la primera app

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from platzigram.views import hello_world,hi, challenge_numbers, say_hi
+from platzigram import views as local_views
+from posts import views as posts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello-world', hello_world),
-    path('hi', hi),
-    path('challenge-number', challenge_numbers),
-    path('say-hi/<str:name>/<int:age>/',say_hi)
+    path('hello-world',local_views.hello_world),
+    path('hi', local_views.hi),
+    path('challenge-number', local_views.challenge_numbers),
+    path('say-hi/<str:name>/<int:age>/',local_views.say_hi),
+    # posts views
+    path('posts/',posts_views.list_posts),
 ]
