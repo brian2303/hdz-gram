f02e2e906f69f2753e7ba90de148b365dd6a60b3
Brian olarte <brayan_andrey23@hotmail.com>
2021-03-03 20:16:19 -0500

    Pasando argumentos a las url

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from platzigram.views import hello_world,hi
+from platzigram.views import hello_world,hi, challenge_numbers, say_hi
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello-world', hello_world),
-    path('hi', hi)
+    path('hi', hi),
+    path('challenge-number', challenge_numbers),
+    path('say-hi/<str:name>/<int:age>/',say_hi)
 ]
--- platzigram/views.py
+++ platzigram/views.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from django.http import HttpResponse
+import json
+
 
 # Utilities
 from datetime import datetime
@@ -20,3 +22,17 @@ def hi(request):
     print(request.POST)
     # print(request.META) # Those are the headers of request
     return HttpResponse('Hii!!')
+
+# return a group of numbers sorted
+def challenge_numbers(request):
+    numbers = [int(i) for i in request.GET['numbers'].split(',')] 
+    numbers = sorted(numbers)
+    data = {
+        'status':'ok',
+        'data': numbers
+    }
+    return HttpResponse(json.dumps(data), content_type='application/json')
+
+def say_hi(request, name, age):
+    greets = f'Hola {name} tu edad es {age}'
+    return HttpResponse(greets)
